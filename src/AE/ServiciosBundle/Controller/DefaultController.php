844701c340ffa6b59e84b3c2a695ae2356bdfd3d
root <root@marks-Lenovo-G470>
2013-03-07 12:18:33 -0500

    servicio routing defaultcontrolele

--- src/AE/ServiciosBundle/Controller/DefaultController.php
+++ src/AE/ServiciosBundle/Controller/DefaultController.php
@@ -903,5 +903,20 @@ class DefaultController extends Controller
         
         return new JsonResponse($todo); 
    }
+   
+     public function pordescartarAction()
+   {
+       $em = $this->getDoctrine()->getEntityManager();
+
+
+        $sql = "select * from descartar";
+          
+        $smt = $em->getConnection()->prepare($sql);
+        $smt->execute();
+        
+        $todo = $smt->fetchAll();
+        
+        return new JsonResponse(array('aaData'=>$todo)); 
+   }
  
 }
