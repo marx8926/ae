acb190d92cab158337706e8f9b2fac0a5339e5a0
clm <clm@CLM-NET>
2013-03-08 11:07:22 -0500

    Arreglado para exportar a excel Ajustes en la exportación a PDF

--- web/extensiones/exportpdf.php
+++ web/extensiones/exportpdf.php
@@ -2,32 +2,33 @@
     $tabla = $_POST['content'];
 
     $content ="
-    <style type='text/Css'>
-	<!--
+<style type='text/Css'>
 	table {
-width: 100%;
-border: 1px solid #cef;
-text-align: left; }
-th {
-font-weight: bold;
-background-color: #acf;
-border-bottom: 1px solid #cef; }
-td,th {
-padding: 4px 5px; }
-
-.odd {
-background-color: #def; }
-.odd td {
-border-bottom: 1px solid #cef; }
-    -->
+		width: 90%;
+		border: 1px solid #cef;
+		text-align: left; 
+	}
+	th {
+		font-weight: bold;
+		background-color: #acf;
+		border-bottom: 1px solid #cef; 
+	}
+	
+	.odd {
+		background-color: #def; 
+	}
+	.odd td {
+		border-bottom: 1px solid #cef; 
+	}
 </style>
-	<page >
-    <table>
+	
+<page >
+    		<table>
     		
     ".$tabla."
-    		
-    </table>
-    </page>
+    		</table>
+
+</page>
     ";
 
 
@@ -35,5 +36,5 @@ border-bottom: 1px solid #cef; }
     require_once(dirname(__FILE__).'/html2pdf/html2pdf.class.php');
     $html2pdf = new HTML2PDF('P','A4','es');
     $html2pdf->WriteHTML($content);
-    $html2pdf->Output('exemple.pdf');
+    $html2pdf->Output('example.pdf');
 ?>
