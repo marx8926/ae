61a33f3d5c3d62b1a0877eeb58fc1512cd546720
clm <clm@CLM-NET>
2013-03-25 13:06:53 -0500

    Cambios template y CSS

---
+++ web/js/modal.js
@@ -0,0 +1,12 @@
+// Function to show modal
+$( '#registro' ).on( 'click', function( ev ) {
+  $( '#modal' ).fadeIn();
+  $( '#modal-background' ).fadeTo( 500, .5 );
+  ev.preventDefault();
+} );
+
+// Function to hide modal
+$( '#close-modal' ).on( 'click', function( ev ) {
+  $( '#modal, #modal-background' ).fadeOut();
+  ev.preventDefault();
+} );
